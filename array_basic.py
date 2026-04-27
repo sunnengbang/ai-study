19ed2dc2d7e53596e34f4a8eee8d7644008650d4
sunnengbang <2025214037@bupt.cn>
2026-04-27 21:13:55 +0800

    calculate average

--- array_basic.py
+++ array_basic.py
@@ -1,4 +1,13 @@
 score=[80,90,75]
-print(scores)
+total = 0
+for score in scores:
+  total = total + score
+
+average =total /len(scores)
+
+print("Scores:",score)
+print("total:",total)
+print(average)
+
 print(max(scores))
 print(min(scores))
